d526689b0802ea3826bde4114dcf5a1b97301555
Jesus Martinez <jesusd.martinez@Jesuss-MacBook-Pro.local>
2019-10-18 16:42:07 +0800

    Queue Implementation

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -1,22 +1,28 @@
 class Queue:
     
     def __init__(self):
-        ...
+        self._data = list()
     
     def enqueue(self, item):
         """
-        Add item to the queue.
+         Size:  O(n)
+         Description:  Add item to the queue
+         Parameter:  "item" identify the item to add
         """
-        ...
+        self._data.insert(0, item)
             
     def dequeue(self):
         """
-        Remove item from the queue.
+        Size:  O(1)
+        Description:  Remove item from the queue
         """
-        ...
-    
+        item = self._data[0]
+        del self._data[0]
+        return item
+
     def __len__(self):
         """
-        Returns the length of the queue
+        Size:  O(n)
+        Description:  returns the length of the queue
         """
-        ...
+        return len(self._data)
