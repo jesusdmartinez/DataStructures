6d448c42ed590f11017676a43c8ec53732deff7b
Jesus Martinez <jesusd.martinez@Jesuss-MacBook-Pro.local>
2019-10-18 16:41:21 +0800

    Stack Implementation

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -1,16 +1,22 @@
 class Stack:
-    
+    def __init__(self):
+        self._data = list()
+
     def push(self, item):
-        ...
-            
+        self._data.append(item)  # adds an item to the list
+
     def pop(self):
-        ...
-        
+        if len(self._data) == 0:
+            raise Exception
+
+        else:
+            item = self._data[len(self._data) - 1]
+            del self._data[len(self._data) - 1]    # deletes the last item in the list
+            return item
+
     def peek(self):
-        ...
+        return self._data[len(self._data) - 1]    # returns the last item in the list
         
     def __len__(self):
-        """
-        Returns the length of the list
-        """
-        ...
+        return len(self._data)    # returns the length of the list
+
